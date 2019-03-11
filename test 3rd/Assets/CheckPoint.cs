365993f267209bd27d1f936bd1389202c82b7dc0
JAROSSET Corentin <corentin.jarosset@isen.yncrea.fr>
2019-03-11 14:14:28 +0100

    checkpoints

---
+++ test 3rd/Assets/CheckPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    DeathZone deathzone;
+    void Start()
+    {
+        deathzone = GameObject.Find("DeathZone").GetComponent<DeathZone>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.tag == "Player")
+        {
+            deathzone.respawnPosition = gameObject.transform.position;
+        }
+    }
+}
---
+++ test 3rd/Assets/DeathZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public Vector3 respawnPosition;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.tag == "Player")
+        {
+            other.transform.position = respawnPosition;
+        }
+    }
+}
