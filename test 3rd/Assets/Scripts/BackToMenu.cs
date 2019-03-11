3f364b91a57eff72507084c6b59bfd524630249d
JAROSSET Corentin <corentin.jarosset@isen.yncrea.fr>
2019-03-11 15:09:57 +0100

    menu + fin

---
+++ test 3rd/Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    public GameObject Player;
+    public int sceneIndex;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
